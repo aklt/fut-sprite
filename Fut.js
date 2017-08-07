c02365bddb58d42a4a39ff22edf4554dafef4bf5
Anders Thøgersen <anders@bladre.dk>
2017-08-07 18:08:24 +0200

    WIP

--- Fut.js
+++ Fut.js
@@ -18,12 +18,12 @@ function spriteParseOne(str, prev) {
   result.x = intOfChar(str[1], prev.x || 0)
   result.y = intOfChar(str[2], prev.y || 0)
   result.c = intOfChar(str[3], prev.c || 0)
-  result.sz = intOfChar(str[4], prev.sz || 18)
+  result.sz = intOfChar(str[4], prev.sz || 17)
   result.r = intOfChar(str[5], prev.r || 0)
   result.sx = intOfChar(str[6], prev.sx || 1)
   result.sy = intOfChar(str[7], prev.sy || 1)
   result.mode = intOfChar(str[8], prev.mode || 0)
-  result.paint = result.mode === 1 ? 'stroke' : 'fill'
+  result.paint = result.mode ? 'stroke' : 'fill'
   return result
 }
 
@@ -44,6 +44,7 @@ function splitAt(str, length) {
 /**
  * @brief  parse all sprite strings into an array of objects
  *
+ * @inline
  * @param {string} ss
  * @return {Array<Object>}
  */
@@ -57,50 +58,54 @@ function spriteParse(ss) {
 }
 
 /**
- * @export
- */
-class Fut {
-  /**
-  * @param {string} chars
-  * @param {Array<string>} pal
+  * @constructor
   * @param {string} vars
   */
-  constructor(chars, pal, vars) {
-    this.chars = chars
-    this.pal = pal
-    this.v = spriteParse(vars)
-  }
+function Fut(vars) {
+  var o = vars.split('~')
+  this.chars = o[0]
+  this.pal = o[1].split(/\|/)
+  this.v = spriteParse(o[2])
+}
 
-  /**
-   * @param {CanvasRenderingContext2D} ctx
-   * @param {number} x
-   * @param {number} y
-   */
-  paint(ctx, x, y) {
+/**
+ * @param {CanvasRenderingContext2D} ctx
+ * @param {number} x
+ * @param {number} y
+ */
+Fut.prototype['paint'] = function(ctx, x, y) {
+  ctx.save()
+  // ctx.textBaseline = 'middle'
+  // ctx.textAlign = 'center'
+  for (var i = 0; i < this.v.length; i += 1) {
+    var o = this.v[i]
+    var ox = o.x - 17
+    var oy = o.y - 17
+    // Scale up to 4,5 the original size
+    var sx = (o.sx - 17)
+    var sy = (o.sy - 17)
+    var tx = (ox + x) / sx
+    var ty = (oy + y) / sy
     ctx.save()
-    ctx.textBaseline = 'middle'
-    ctx.textAlign = 'center'
-    for (var i = 0; i < this.v.length; i += 1) {
-      var o = this.v[i]
-      var ox = o.x - 18
-      var oy = o.y - 18
-      // Scale up to 4,5 the original size
-      var sx = (o.sx - 17) / 4
-      var sy = (o.sy - 17) / 4
-      var tx = (ox + x) / sx
-      var ty = (oy + y) / sy
-      ctx.save()
-      ctx.font = o.sz + 'px arial'
-      ctx.scale(sx, sy)
-      // Rotate
-      ctx.translate(tx, ty)
-      ctx.rotate(Math.PI / 180 * (360 / 36) * o.r)
-      ctx.translate(-tx, -ty)
-      ctx[o.mode + 'Style'] = '#' + this.pal[o.c].slice(0, 6)
-      ctx[o.mode + 'Text'](this.chars[o.char], tx, ty)
-      ctx.restore()
-    }
+    ctx.font = o.sz + 'px Arial'
+    ctx.scale(sx, sy)
+    // Rotate
+    ctx.translate(tx, ty)
+    ctx.rotate(Math.PI / 180 * (360 / 36) * o.r)
+    ctx.translate(-tx, -ty)
+    ctx[o.paint + 'Style'] = '#' + this.pal[o.c].slice(0, 6)
+    ctx.globalAlpha = parseInt(this.chars[o.char].slice(6), 16) / 0xff;
+    ctx[o.paint + 'Text'](this.chars[o.char], tx, ty)
     ctx.restore()
+  }
+  ctx.restore()
+}
+
+Fut.prototype['tick'] = function (group) {
+  if (this.groupFuns && this.groupFuns[group]) {
+    // FIXME collect images that are subject to tick()
+    var imgs = []
+    this.groupFuns[group](imgs)
   }
 }
 
